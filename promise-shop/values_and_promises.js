d5f29fd7ac2a1c710906f6d77ee986f1138385f4
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-02-13 19:34:09 -0300

    encadeno promises, esa es la idea, retorno un promise y el cliente se encarga de atacharle el callback

---
+++ promise-shop/values_and_promises.js
@@ -0,0 +1,7 @@
+var q = require('q');
+var defer = q.defer();
+var attachTitle = function(name){
+	return "DR. " + name;
+};
+defer.promise.then(attachTitle).then(console.log);
+defer.resolve("MANHATTAN");
